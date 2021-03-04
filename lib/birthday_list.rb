65777a11b21a0201a512ef770d26c5d6693bec77
Lisa <bardelli.lisa@gmail.com>
2021-03-04 18:15:40 +0000

    Add solution

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,1 +1,25 @@
+require 'date'
+class Friends
+    attr_accessor :name, :friends_list, :birthday_date
+    def initialize(name,birthday_date )
+        @name = name
+        @friends_list = {}
+        @birthday_date = birthday_date
+    end
+    def list_friends
+        @friends_list.append(@name, @birthday)
+        return @friends_list.join(',')
+    end
+    def print 
+        puts @friends_list
+    end
+    def return_date
+        puts Date.today
+    end
+end
 
+
+person = Friends.new("carlo", "01-05-2018")
+person = Friends.new("Mirco", '01-05-2019')
+puts person.list_friends
+puts person.return_date
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,1 +1,29 @@
+require "birthday_list"
+require 'date'
+
+describe Friends do
+  let(:person) {Friends.new 'Carlo', "2018-11-27"}
+  
+  it "creates an instance of Friends class  with name" do
+    expect(person.name).to eq "Carlo"
+  end
+  it "creates an instance of Friends class with birthday_date" do
+    expect(person.birthday_date).to eq "2018-11-27" 
+  end
+ 
+  it "list_friends without raising an error" do
+    expect{person.list_friends}.to_not raise_error
+  end
+  it "return name and dirthday" do
+    person = Friends.new('Mimmo', "11-02-1959")
+  expect(person.list_friends).not_to be_empty
+  end
+
+  # it "returns birtday equal to today_date" do
+  #   expect(friend.return_date).to eq(Date.today) 
+  # end
+
+
+  end
+
 
